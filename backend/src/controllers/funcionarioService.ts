9fc7ab4bb7295176712e0526c9fc0f8bde877baf
Gustavobiz <gustavobernardes05@hotmail.com>
2025-07-03 16:46:05 -0300

    feat: funcionarioService

---
+++ backend/src/controllers/funcionarioService.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export const criarFuncionario = async (dados: {
+  nome: string;
+  sobrenome: string;
+  cpf: string;
+  cargo: string;
+  data_admissao: Date;
+  usuario_id: number;
+}) => {
+  return await prisma.funcionario.create({
+    data: dados,
+  });
+};
