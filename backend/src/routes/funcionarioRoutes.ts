c0196237ddb1af9abbd1d7424fa1a5921aa5a9f2
Gustavobiz <gustavobernardes05@hotmail.com>
2025-07-08 18:31:48 -0300

    feat: rota GET /funcionarios/:id já funcional

--- backend/src/routes/funcionarioRoutes.ts
+++ backend/src/routes/funcionarioRoutes.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { postFuncionario } from "../controllers/funcionarioController";
+import { getFuncionarioPorId } from "../controllers/funcionarioController";
 
 const router = express.Router();
 
 router.post("/new", postFuncionario);
+
+router.get("/:id", getFuncionarioPorId);
 
 export default router;
