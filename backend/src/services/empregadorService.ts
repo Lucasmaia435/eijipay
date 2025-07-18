d7e7a7d6ed8055b719db9ab24bbfb60ecd1bc645
Elildes Fortaleza Santos <colafina2000@yahoo.com.br>
2025-07-18 18:02:12 -0300

    fix-bd: restaura o banco de dados

--- backend/src/services/empregadorService.ts
+++ backend/src/services/empregadorService.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
 // Instancia o Prisma Client, passando a extensão Accelerate
