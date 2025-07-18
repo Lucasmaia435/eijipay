6e8a888b1a726e535e76f1d63abb7970029ef581
Gustavobiz <gustavobernardes05@hotmail.com>
2025-07-17 23:32:42 -0300

    feat: empregador rotas testadas

--- backend/src/routes/empregadorRoutes.ts
+++ backend/src/routes/empregadorRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import * as controller from "../controllers/empregadorController";
+import { verificarToken } from "../middlewares/authMiddleware";
 
 const router = Router();
 
-router.get("/", controller.getAll);
-router.get("/:cnpj", controller.getByCnpj);
-router.post("/", controller.create);
-router.put("/:cnpj", controller.update);
-router.delete("/:cnpj", controller.remove);
+router.get("/", verificarToken, controller.getAll);
+router.get("/:cnpj", verificarToken, controller.getByCnpj);
+router.post("/", verificarToken, controller.create);
+router.put("/:cnpj", verificarToken, controller.update);
+router.delete("/:cnpj", verificarToken, controller.remove);
 
 export default router;
